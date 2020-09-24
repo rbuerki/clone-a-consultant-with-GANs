d49824c82e8b789a1d32e6984fecc4cf76b9da4f
rbuerki <r2d4@bluewin.ch>
2020-09-24 13:22:09 +0200

    Refactor, pass test

--- preprocess_img.py
+++ preprocess_img.py
@@ -1,8 +1,8 @@
 import configparser
 import json
-import os
 from io import BytesIO
-from typing import Dict, Tuple
+from typing import Dict, Optional, Tuple
+from pathlib import Path
 
 import cv2
 import numpy as np
@@ -10,33 +10,56 @@ import PIL
 import requests
 from PIL import Image
 
-# TODO this has to got in the executing function
-# config = configparser.ConfigParser()
-# config.read("config.cfg")
 
-# FULL_URL = config.get("WEBSITE", "FULL_URL")
-# BASE_URL = config.get("WEBSITE", "BASE_URL")
-# FACE_CASCADE_LOCATION = config.get("RESOURCES", FACE_CASCADE_LOCATION)
-
-
-def scrape_people_content_from_website(full_url: str) -> Dict:
-    """Return a dict containing the website's content from the
-    `people` section. It's made up of personal information and
-    a path to a picture for each employee.
+def parse_config_img_data(path_to_cfg: str) -> Tuple[str, str, str, str, str]:
+    """Retrieve necessary config stuff for image retreival
+    and preprocessing.
     """
-    people_json = requests.get(full_url)
-    people_dict = json.loads(people_json.text)
-    return people_dict
+    config = configparser.ConfigParser()
+    try:
+        config.read(path_to_cfg)
+    except FileNotFoundError as e:
+        raise (f"Please check the path to the config file: {e}")
+
+    full_url = config.get("SCRAPING", "FULL_URL")
+    base_url = config.get("SCRAPING", "BASE_URL")
+    path_to_fc = config.get("IMG_PROCESS", "FACE_CASCADE_RELPATH")
+    scale_factor = float(config.get("IMG_PROCESS", "SCALE_FACTOR"))
+    dsize = tuple(json.loads(config.get("IMG_PROCESS", "DSIZE")))
+    return full_url, base_url, path_to_fc, scale_factor, dsize
 
 
-def generate_full_link_to_picture(
-    i: int, people_dict: Dict, base_url: str
+def scrape_data_from_website(full_url: str) -> Dict:
+    """Return a dict containing the target website's content. (Here
+    from its `people` section. It's made up of personal information,
+    containing a path to a picture for each employee.)
+    """
+    response_json = requests.get(full_url)
+    response_dict = json.loads(response_json.text)
+    return response_dict
+
+
+def instantiate_OpenCV_face_detector(path_to_fc: str) -> cv2.CascadeClassifier:
+    """Load pre-trained Haar feature-based cascade classifier
+    from a stored XML file and instantiate it.
+    """
+    abs_path_to_fc = Path(path_to_fc).absolute()
+    if not Path.is_file(abs_path_to_fc):
+        raise AssertionError(
+            f"No valid path for classifier.xml at {abs_path_to_fc}"
+        )
+    face_cascade = cv2.CascadeClassifier(str(abs_path_to_fc))
+    return face_cascade
+
+
+def generate_full_link_to_image(
+    i: int, response_dict: Dict, base_url: str
 ) -> str:
-    """Parse dict to get the path to the picture for the employee at
+    """Parse dict to get the path to the image for the employee at
     position i and return the full link (combined with the URL base).
     """
     # for i, employee in people_dict["modules"]:
-    path_to_pic = people_dict["modules"][i]["data"]["media"]["source"]
+    path_to_pic = response_dict["modules"][i]["data"]["media"]["source"]
     link_to_image = base_url + path_to_pic
     return link_to_image
 
@@ -53,50 +76,46 @@ def load_image_PIL(
         pil_rgb = Image.open(BytesIO(r.content))
         return pil_rgb
     except PIL.UnidentifiedImageError:
-        return None  # TODO continue whole loop then ...
+        return None
 
 
-def convert_image_PIL_to_cv_gray(
+def convert_image_PIL_to_cv_gray_and_rgb(
     pil_rgb: PIL.JpegImagePlugin.JpegImageFile,
-) -> np.ndarray:
+) -> Tuple[np.ndarray, np.ndarray]:
     """Return grayscale image in form of an numpy array (the image
     format OpenCV uses). Grayscale is the default for face detection
     in OpenCV. Note also that, when there's color, OpenCV does use
-    an inverted BGR scale, so we flip that first.
+    an inverted BGR scale, so we flip that first. And final note:
+    No transformation back from grey to color, so we need both.
     """
     cv_rgb = np.array(pil_rgb)
     cv_bgr = np.flip(cv_rgb, axis=-1)  # faster subsitute for cv2.RBG2BGR
     cv_gray = cv2.cvtColor(cv_bgr, cv2.COLOR_BGR2GRAY)
-    return cv_gray
+    return cv_gray, cv_rgb
 
 
-def locate_face_in_image(
-    cv_gray: np.ndarray, i: int, face_cascade_loc: str
-) -> Optional[Tuple(int, int, int, int)]:
-    """Extract the pre-trained face detector, run it, and return the
-    coordinates (x,y,w,h) for the the first rectangle containing a
-    detected face in the list. (Note: There should never be more than
-    one person on a picture, if more faces are detected we will simply
-    use the first in the list.) Return None if no face is detected.
+def detect_face_in_image(
+    cv_gray: np.ndarray, face_cascade: cv2.CascadeClassifier,
+) -> Tuple[Tuple[Optional[int]], int]:
+    """Run the face detector, and return the coordinates (x,y,w,h) for
+    the the FIRST rectangle containing a detected face in the list.
+    (Note: There should never be more than one person on a picture,
+    if more faces are detected we will simply use the first in the
+    list.)If no face is detected, the tuple is empty. Also return the
+    number of faces found.
     """
-    face_cascade = cv2.CascadeClassifier(face_cascade_loc)
     faces = face_cascade.detectMultiScale(cv_gray)
-
-    if len(faces) == 0:
-        print(f"NO face detected for image nr {i}")
-        return None  # TODO continue that whole loop then
+    n_faces = len(faces)
+    if n_faces == 0:
+        cv_box = faces
     else:
-        if len(faces) > 1:
-            print(
-                f"More than 1 face detected in image nr {i}, will use first only."
-            )
         cv_box = tuple(faces[0])
-        return cv_box
+    return cv_box, len(faces)
 
 
 def crop_image(
-    cv_gray: np.ndarray(),
-    cv_box: Tuple(int, int, int, int),
+    cv_gray_in: np.ndarray,
+    cv_box: Tuple[int, int, int, int],
     scale_factor: float,
 ) -> np.ndarray:
     """Return a cropped square cutout of the original grayscale image
@@ -108,32 +127,64 @@ def crop_image(
     y_delta = int(max((scale_factor - 1) * h, 0))
 
     # Use np.array slicing for the cropping
-    face_gray = cv_gray[
+    cv_gray_out = cv_gray_in[
         y - y_delta : y + y_delta + h, x - x_delta : x + x_delta + w
     ].copy()
-    return face_gray
-
-
-def convert_image_cv_gray_to_cv_rgb(face_gray: np.ndarray) -> np.ndarray:
-    """Convert cv image (numpy array) from grayscale to RGB
-    color scale. (This will be used as training data.)
-    """
-    face_rbg = cv2.cvtColor(face_gray, cv2.COLOR_GRAY2RGB)
-    return face_rbg
+    return cv_gray_out
 
 
 def resize_image_cv(
-    face_rbg: np.ndarray, dsize: Tuple(int, int) = None
+    cv_in: np.ndarray, dsize: Tuple[int, int] = None
 ) -> np.ndarray:
-    """Resize image to given size. If no dsize tuple (width, 
+    """Resize image to given size. If no dsize tuple (width,
     height in pixels) is passed, the size does not change.
     """
     if dsize:
-        face_final = cv2.resize(face_rbg, dsize)
+        cv_out = cv2.resize(cv_in, dsize)
     else:
-        face_final = face_rbg
-    return face_final
+        cv_out = cv_in
+    return cv_out
 
 
 def save_final_image(face_final: np.ndarray, dir_path: str, i: int):
     pass
+
+
+def main(path_to_cfg):
+    """[summary]
+    """
+    count_invalid_img = count_no_face_img = count_multi_face_img = 0
+
+    full_url, base_url, path_to_fc, scale_factor, dsize = parse_config_img_data(
+        path_to_cfg
+    )
+    employee_data = scrape_data_from_website(full_url)
+    face_cascade = instantiate_OpenCV_face_detector(path_to_fc)
+
+    for i, _ in enumerate(employee_data["modules"]):
+        link_to_image = generate_full_link_to_image(i, employee_data, base_url)
+        pil_rgb = load_image_PIL(link_to_image)
+        if pil_rgb is None:
+            count_invalid_img += 1
+            continue
+        else:
+            cv_gray, cv_rgb = convert_image_PIL_to_cv_gray_and_rgb(pil_rgb)
+            cv_box, n_faces = detect_face_in_image(cv_gray, face_cascade)
+            if n_faces == 0:
+                print(f"NO face detected for image nr {i}.")
+                count_no_face_img += 1
+                continue
+            else:
+                if n_faces > 1:
+                    print(
+                        f"More than 1 face detected in image nr {i},",
+                        "will use first only.",
+                    )
+                    count_multi_face_img += 1
+                face_rgb = crop_image(cv_rgb, cv_box, scale_factor=1.1)
+                face_final = resize_image_cv(face_rgb, dsize)
+                return face_final
+
+
+if __name__ == "__main__":
+    main("config.cfg")
